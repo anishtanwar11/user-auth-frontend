e7511c7b5eb742c763c97c969c4be70c5db113e0
Anish <anishtanvar@gmail.com>
2024-10-24 13:27:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center text-[4rem] text-emerald-700 font-bold'>
+      <h1>User Authentication App</h1>
+    </div>
+  )
+}
+
+export default App
