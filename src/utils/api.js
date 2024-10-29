558564f18f3adb80f59f0a4e75311a4934c36178
Anish <anishtanvar@gmail.com>
2024-10-29 21:39:07 +0530

    Change backend URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8000/api/v1";
+// const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://user-auth-backend-omega.vercel.app/api/v1"
 
 export const API_ENDPOINTS = {
     register: `${BASE_URL}/user/register`,
