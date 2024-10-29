30eb12880bc394606a8d1dce7848b3b9fc9f2d0a
Anish <anishtanvar@gmail.com>
2024-10-29 21:59:31 +0530

    Correct logout request

--- src/components/reuseable/LogoutButton.jsx
+++ src/components/reuseable/LogoutButton.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 import { API_ENDPOINTS } from "../../utils/api";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/slices/authSlice";
@@ -7,19 +6,20 @@ import { logout } from "../../store/slices/authSlice";
 const LogoutButton = () => {
 
   const dispatch = useDispatch();
-  
-    const handleLogout = async () => {
-        try {
-            const response = await axios.post(API_ENDPOINTS.logout, {} , {
-                withCredentials: true
-            }) ;
 
-            dispatch(logout())
-            console.log(response.data);
-        } catch (error) {
-            console.log("Logout error", error)
-        }
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post(API_ENDPOINTS.logout, {}, {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true
+      });
+
+      dispatch(logout())
+      console.log(response.data);
+    } catch (error) {
+      console.log("Logout error", error)
     }
+  }
 
   return (
     <div>
